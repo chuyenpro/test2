8a9297d3ae52e40199206ecd792be32cc9e07263
chuyenpro <115093851+chuyenpro@users.noreply.github.com>
2023-03-09 10:34:43 +0700

    m

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useRef, useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./Home.module.scss";
-import Author from "../Author/Author";
+
 import { Routes, Route, Link } from "react-router-dom";
 import { GiPerspectiveDiceSixFacesRandom } from "react-icons/gi";
+import {GrFormNextLink} from 'react-icons/gr'
 
 const cx = classNames.bind(styles);
 const Home = () => {
   const [showQuotes, setShowQuotes] = useState([]);
   const [indexRandom, setIndexRandom] = useState([]);
   const [random, setRandom] = useState(0);
+  const [author, setAuthor]= useState([])
+  const [showAuthor, setShowAuthor]=useState(false)
 
   useEffect(() => {
     fetch("https://api.quotable.io/quotes")
@@ -17,8 +20,6 @@ const Home = () => {
       .then((data) => {
         setShowQuotes(data.results[0]);
         setIndexRandom(data.results);
-
-        console.log(showQuotes);
       });
   }, []);
 
@@ -32,6 +33,20 @@ const Home = () => {
         setIndexRandom(data.results);
       });
   };
+
+  const [authorQuotes, setAuthorQuotes] = useState([]);
+
+  const [showAuthorQuotes, setShowAuthorQuotes] = useState(false);
+
+  const handleAuthorQuotes = (author) => {
+    setShowAuthorQuotes(true);
+    setShowQuotes(false);
+    const quotesByAuthor = indexRandom.filter(
+      (quote) => quote.author === author
+    );
+    setAuthorQuotes(quotesByAuthor);
+  };
+
   return (
     <>
       <div className={cx("wrapper")}>
@@ -42,9 +57,26 @@ const Home = () => {
         <div className={cx("content")}>
           <p className={cx("text")}>{showQuotes.content}</p>
           <div className={cx("author")}>
-            <Link to='/author' className={cx("author-btn")}>{showQuotes.author}</Link>
+            <button className={cx("author-btn")} onClick={()=>handleAuthorQuotes(showQuotes.author)}>{showQuotes.author}
+            <GrFormNextLink className={cx('icon')}/></button>  
             <p>{showQuotes.tags}</p>
           </div>
+
+          <ul>
+          {authorQuotes.map((quote) => (
+            <li style={{
+              listStyle:'none'
+            }} key={quote._id}>
+              <h3>{quote.author}</h3>
+              <h4 style={{
+                paddingLeft: '20px',
+                borderLeft:'3px solid sandybrown ',
+                marginBottom: '30px' 
+              }}>{quote.content}</h4>
+              <h5>{quote.tags}</h5>
+            </li>
+          ))}
+        </ul>
         </div>
       </div>
     </>
